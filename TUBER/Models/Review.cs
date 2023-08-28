64d47059221d83018e69a5fd9ef8ed1bc25f9030
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-08-28 15:40:17 -0600

    Review Model

---
+++ TUBER/Models/Review.cs
@@ -0,0 +1,10 @@
+namespace TUBER.Models;
+
+public class Review
+{
+  public int Id { get; set; }
+  public int RatingCount { get; set; }
+  public string StudentId { get; set; }
+  public string TutorId { get; set; }
+
+}
--- TUBER/Models/Topic.cs
+++ TUBER/Models/Topic.cs
@@ -2,6 +2,7 @@ namespace TUBER.Models;
 
 public class Topic
 {
+  public int Id { get; set; }
   public string Subject { get; set; }
   public int HourlyRate { get; set; }
   public string AccountId { get; set; }
