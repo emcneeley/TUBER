5551d34730f442d26c323719aad33a567e5ec702
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-08-28 15:30:40 -0600

    Topic Model

---
+++ TUBER/Models/Topic.cs
@@ -0,0 +1,9 @@
+namespace TUBER.Models;
+
+public class Topic
+{
+  public string Subject { get; set; }
+  public int HourlyRate { get; set; }
+  public string AccountId { get; set; }
+  public string Level { get; set; }
+}
